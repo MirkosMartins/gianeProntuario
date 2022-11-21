f2f14f327d7645a5e842ef516ef5b3672b3d806b
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-11-21 17:24:45 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -5,3 +5,4 @@ st.title('Predição de gravidade COVID a partir de Prontuário')
 st.header('(c)2022 - Giane Engel / Mirkos Martins')
 
 genero = st.selectbox('Selecione o gênero do paciente:',('masculino','feminino'))
+idade = st.number_input('Digite a idade do paciente:',min_value=1,max_value=150,step=1)
