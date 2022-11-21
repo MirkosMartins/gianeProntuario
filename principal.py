5d1a2ac7b6b55350c963075f5056f97aef9715db
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-11-21 17:21:05 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -4,4 +4,4 @@ import pandas as pd
 st.title('Predição de gravidade COVID a partir de Prontuário')
 st.header('(c)2022 - Giane Engel / Mirkos Martins')
 
-genero = st.select_box('Selecione o gênero do paciente:',('masculino','feminino'))
+genero = st.selectbox('Selecione o gênero do paciente:',('masculino','feminino'))
