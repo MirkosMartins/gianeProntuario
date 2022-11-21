20b2f52c2a6e04e200650d674d14842cd177abe2
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-11-21 17:32:24 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -8,4 +8,7 @@ genero = st.selectbox('Selecione o gênero do paciente:',('masculino','feminino'
 idade = st.number_input('Digite a idade do paciente:',min_value=1,max_value=150,step=1)
 RDW = st.number_input('RDW (%)')
 leucocitos = st.number_input('Leucocitos (x109/L)',step=0.1)
-
+monocitos = st.number_input('Monócitos (x109/L)',step=0.001)
+linfocitos = st.number_input('Linfócitos (x109/L)',step=0.001)
+plaquetas = st.number_input('Plaquetas (x109/L)',step=1000)
+neutrofilos = st.number_input('Neutrófilos (x109/L)',step=0.001)
